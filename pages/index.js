11b6e3d9f6e0f8f647a7c44e89ff6a8223c3de82
daja-w <104468175+daja-w@users.noreply.github.com>
2023-12-30 01:45:20 -0700

    create primary layout

--- pages/index.js
+++ pages/index.js
@@ -2,37 +2,41 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { useSession } from 'next-auth/react'
 import { useEffect, useState } from 'react'
+import Sidebar from '@/components/Sidebar'
+import PlaylistView from '@/components/PlaylistView'
+import Search from '@/components/Search'
+import Library from '@/components/Library'
+import Artist from '@/components/Artist'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const {data:session}= useSession()
-  const [x, setX] = useState('')
-  const [playlists, setPlaylists] = useState([])
-  // console.log(session.user.accessToken)
-
-  useEffect(() =>{
-    async function f() {
-      if (session && session.accessToken){
-        setX(session.accessToken)
-        const response = await fetch("https://api.spotify.com/v1/me/playlists", {
-          headers: {
-            Authorization: `Bearer ${session.accessToken}`
-          }
-        })
-        const data = await response.json()
-        setPlaylists(data.items)
-      }
-    }
-    f()
-  }, [session])
+  const [view, setView] = useState("search") // ["search", "library", "playlist", "artist"]
+  const [globalPlaylistId, setGlobalPlaylistId] = useState(null)
+  const [globalArtistId, setArtistId] = useState(null)
 
   return (
-    <main className='flex flex-col items-center w-full'>
-      <div>access token: {x}</div>
-      <div>
-        {playlists.map((playlist) => <div key={playlist.id}>{playlist.name}</div>)}
-      </div>
+    <>
+    <main className='h-screen overflow-hidden bg-black'>
+      <div className='flex w-full'>
+        <Sidebar
+          view={view}
+          setView={setView} // this is how sidebar component will be able to change the main view
+          setGlobalPlaylistId={setGlobalPlaylistId}
+
+        />
+        {view === "playlist" && <PlaylistView
+          globalPlaylistId={globalPlaylistId}
+        />}
+        {view === "search" && <Search/>}
+        {view === "library" && <Library/>}
+        {view === "artist" && <Artist/>}
+       </div>
+
+
+
     </main>
+    <div className='sticky z-20 bottom-0 h-24 w-full bg-red-100'></div>
+    </>
   )
 }
