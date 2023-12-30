dbb6d41f82b935a39c7bdd39b54199599604d8ff
daja-w <104468175+daja-w@users.noreply.github.com>
2023-12-29 19:20:19 -0700

    config and wrap app in SessionProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import { SessionProvider } from "next-auth/react"
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+//wrap application in session provider
+//allowing
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
