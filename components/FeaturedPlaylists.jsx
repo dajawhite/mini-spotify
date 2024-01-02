35d8d5a10be77328e1481b40269e5ee212551b40
daja-w <104468175+daja-w@users.noreply.github.com>
2024-01-01 22:22:27 -0700

    idek

--- components/FeaturedPlaylists.jsx
+++ components/FeaturedPlaylists.jsx
@@ -15,15 +15,15 @@ export const FeaturedPlaylists = ({setView, setGlobalPlaylistId}) => {
     useEffect(() =>{
         async function f() {
           if (session && session.accessToken){
-            const response = await fetch("https://api.spotify.com/v1/browse/featured-playlists" + new URLSearchParams({
+            const response = await fetch("https://api.spotify.com/v1/browse/featured-playlists?" + new URLSearchParams({
                 country: "US"
             }), {
               headers: {
                 Authorization: `Bearer ${session.accessToken}`
               }
             })
-            //const data = await response.json()
-            //setPlaylists(data.playlists.items)
+            const data = await response.json()
+            setPlaylists(data.playlists.items)
           }
         }
         f()
@@ -34,7 +34,7 @@ export const FeaturedPlaylists = ({setView, setGlobalPlaylistId}) => {
         <h2 className='text-xl font-bold'>Featured Playlists</h2>
         <div className='flex flex-wrap gap-6 mb-48'>
           {playlists.map((playlist) =>{
-            return <div onClick={()=>selectPlaylist(playlist)} key={playlist.id} className='cursor-pointer relative group w-56 mb-2 bg-neutral-800 hover:bg-neutral-700 rounded-md p-4'>
+            return <div onClick={()=>selectPlaylist(playlist)} key={playlist.id} className='cursor-pointer relative group w-56 mb-2 bg-neutral-800 hover:bg-neutral-600 rounded-md p-4'>
               {/* group-hover means when the parent with group is hovered over this element will increase opacity to 100 */}
               <div className='absolute opacity-0 group-hover:opacity-100 transition-all ease-in-out duration-200 shadow-2xl shadow-neutral-900 z-10 h-12 w-12 flex items-center  justify-center rounded-full bg-green-500 top-[156px] group-hover:top-[148px] right-6'>
                 <PlayIcon className='h-6 w-6 text-black'/>
