9ea85ec2b021a58c309faface0d2e5b18112dec9
daja-w <104468175+daja-w@users.noreply.github.com>
2024-01-01 22:19:18 -0700

    add middleware

---
+++ middleware.js
@@ -0,0 +1,34 @@
+import { getToken } from 'next-auth/jwt'
+import { NextResponse } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export async function middleware(req) {
+    const token = await getToken({req, secret: process.env.JWT_SECRET})
+
+    //extract pathname from the request
+    const {pathname} = req.nextUrl
+    if( pathname.includes("/api/auth") || token){ // api/auth path or find valid token
+        return NextResponse.next() //leave request as is
+    }
+
+    if(!token && pathname != "/login"){
+        return NextResponse.redirect(new URL('/login', req.url))
+    }
+
+    return NextResponse.next() //default
+    //
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - api (API routes)
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         */
+        '/((?!api|_next/static|_next/image|favicon.ico).*)',
+      ],
+}
