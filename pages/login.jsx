576fd83f6861e106b10a24d927f1c075578ad963
daja-w <104468175+daja-w@users.noreply.github.com>
2024-01-01 22:24:57 -0700

    style login page

--- pages/login.jsx
+++ pages/login.jsx
@@ -4,8 +4,8 @@ import { signIn } from "next-auth/react"
 
 const Login = () => {
     return (
-        <div className="flex items-center justify-center">
-            <button onClick={() => signIn('spotify',{ callbackUrl: "/"})}>Login with Spotify</button>
+        <div className="w-full h-screen flex items-center justify-center">
+            <button className="text-white px-8 py-2 rounded-full bg-green-500 font-bold" onClick={() => signIn('spotify',{ callbackUrl: "/"})}>Login with Spotify</button>
         </div>
     )
 }
