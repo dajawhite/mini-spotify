7abb9eaef3a1b57bce7f56eb8284f6c99146fc56
daja-w <104468175+daja-w@users.noreply.github.com>
2024-01-03 13:13:10 -0700

    remove unecessary imports

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,10 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { useSession } from 'next-auth/react'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import Sidebar from '@/components/Sidebar'
 import PlaylistView from '@/components/PlaylistView'
 import Search from '@/components/Search'
 import Library from '@/components/Library'
 import Artist from '@/components/Artist'
 import Player from '@/components/Player'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const [view, setView] = useState("search") // ["search", "library", "playlist", "artist"]
@@ -21,50 +16,60 @@ export default function Home() {
 
   return (
     <>
-    <main className='h-screen overflow-hidden bg-black'>
-      <div className='flex w-full'>
-        <Sidebar
-          view={view}
-          setView={setView} // this is how sidebar component will be able to change the main view
-          setGlobalPlaylistId={setGlobalPlaylistId}
-
-        />
-        {view === "playlist" && <PlaylistView
-        setView={setView}
-        setGlobalArtistId={setGlobalArtistId}
-          globalPlaylistId={globalPlaylistId}
-          setGlobalCurrentSongId={setGlobalCurrentSongId}
-          setGlobalIsTrackPlaying={setGlobalIsTrackPlaying}
-        />}
-        {view === "search" && <Search 
-        setView={setView} // this is how sidebar component will be able to change the main view
-        setGlobalPlaylistId={setGlobalPlaylistId}
-        setGlobalCurrentSongId={setGlobalCurrentSongId}
-          setGlobalIsTrackPlaying={setGlobalIsTrackPlaying}
-          setGlobalArtistId={setGlobalArtistId}
-          />}
-        {view === "library" && <Library
-        setView={setView} // this is how sidebar component will be able to change the main view
-        setGlobalPlaylistId={setGlobalPlaylistId}/>}
-        {view === "artist" && <Artist
-        setView={setView}
-        globalArtistId={globalArtistId}
-        setGlobalArtistId={setGlobalArtistId}
-        setGlobalCurrentSongId={setGlobalCurrentSongId}
-        setGlobalIsTrackPlaying={setGlobalIsTrackPlaying}
-        />}
-       </div>
-       <div className='sticky z-20 bottom-0 h-24 w-full'>
-        <Player 
-        globalCurrentSongId={globalCurrentSongId}
-        setGlobalCurrentSongId={setGlobalCurrentSongId}
-        setGlobalIsTrackPlaying={setGlobalIsTrackPlaying}
-        globalIsTrackPlaying={globalIsTrackPlaying}/>
-      </div>
-
-
-
-    </main>
+      <main className='h-screen overflow-hidden bg-black'>
+        <div className='flex w-full'>
+          <Sidebar
+            view={view}
+            setView={setView} // this is how sidebar component will be able to change the main view
+            setGlobalPlaylistId={setGlobalPlaylistId}
+          />
+          {
+            view === "playlist" && 
+            <PlaylistView
+              setView={setView}
+              setGlobalArtistId={setGlobalArtistId}
+              globalPlaylistId={globalPlaylistId}
+              setGlobalCurrentSongId={setGlobalCurrentSongId}
+              setGlobalIsTrackPlaying={setGlobalIsTrackPlaying}
+            />
+          }
+          {
+            view === "search" && 
+            <Search 
+              setView={setView} // this is how sidebar component will be able to change the main view
+              setGlobalPlaylistId={setGlobalPlaylistId}
+              setGlobalCurrentSongId={setGlobalCurrentSongId}
+              setGlobalIsTrackPlaying={setGlobalIsTrackPlaying}
+              setGlobalArtistId={setGlobalArtistId}
+            />
+          }
+          {
+            view === "library" && 
+            <Library
+              setView={setView} // this is how sidebar component will be able to change the main view
+              setGlobalPlaylistId={setGlobalPlaylistId}
+            />
+          }
+          {
+            view === "artist" && 
+            <Artist
+              setView={setView}
+              globalArtistId={globalArtistId}
+              setGlobalArtistId={setGlobalArtistId}
+              setGlobalCurrentSongId={setGlobalCurrentSongId}
+              setGlobalIsTrackPlaying={setGlobalIsTrackPlaying}
+            />
+          }
+        </div>
+        <div className='sticky z-20 bottom-0 h-24 w-full'>
+          <Player 
+            globalCurrentSongId={globalCurrentSongId}
+            setGlobalCurrentSongId={setGlobalCurrentSongId}
+            setGlobalIsTrackPlaying={setGlobalIsTrackPlaying}
+            globalIsTrackPlaying={globalIsTrackPlaying}
+          />
+        </div>
+      </main>
     
     </>
   )
